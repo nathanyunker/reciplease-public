bf09c4b6e86de208e13d92761e90ef0008a3f2be
Nathan Yunker <nathanyunker@rocketmail.com>
2017-07-18 21:19:46 -0700

    removed extraneous code'

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,5 @@
 let recipeName = '';
 
-export function fetchUsers() {
-  return function(dispatch) {
-  	fetch('http://rest.learncode.academy/api/wstern/users', {
-      method: 'get'
-    }).then(function(response) {
-        return response.json();
-    }).then(function(data) { 
-    	dispatch({type: "RECIEVE_USERS", payload: data});
-      console.log('DATA', data)
-    });
-  }
-}
-
 export function fetchRecipes() {
   return function(dispatch) {
   	fetch('http://localhost:3000/recipe', {
@@ -21,7 +8,6 @@ export function fetchRecipes() {
         return response.json();
     }).then(function(data) { 
     	dispatch({type: "RECIEVE_RECIPES", payload: data});
-      console.log('DATA', data)
     });
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,25 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux'
 import App from './app'
-// import appReducer from './reducers'
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import store from "./store.js"
-
-// let middleware = applyMiddleware(thunk, logger);
-// let store = createStore(appReducer, middleware);
-
-// store.dispatch((dispatch) => {
-// 	dispatch({type: "FETCH_USERS_START"});
-// 	fetch('http://rest.learncode.academy/api/wstern/users', {
-//       method: 'get'
-//     }).then(function(response) {
-//         return response.json();
-//     }).then(function(data) { 
-//     	dispatch({type: "RECIEVE_USERS", payload: data});
-//       console.log('DATA', data)
-//     });
-
-// })
  
 render(
   <Provider store={store}>
