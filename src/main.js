57329579f1cd349557117949864058c28f34f0a3
Nathan Yunker <nathanyunker@rocketmail.com>
2017-09-24 01:56:19 -0700

    Troubleshooting

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux'
-import App from './app.js'
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import store from "./store.js"
  
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+	<div>Something</div>
   document.getElementById('root')
 )
