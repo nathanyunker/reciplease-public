c679aa0610453e3aa3da1509bde7f13676344bcf
Nathan Yunker <nathanyunker@rocketmail.com>
2017-09-24 01:49:52 -0700

    Troubleshooting

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux'
+import App from './app.js'
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import store from "./store.js"
  
