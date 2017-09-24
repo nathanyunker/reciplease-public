9de01b00455b17f1457d0bc97d3a4dddec24bc4f
Nathan Yunker <nathanyunker@rocketmail.com>
2017-09-24 02:08:46 -0700

    Troubleshooting

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux'
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import store from "./store.js"
+
+const testFolder = 'app/src';
+const fs = require('fs');
+
+fs.readdirSync(testFolder).forEach(file => {
+  console.log('THE FILE', file);
+})
  
 render(
   <Provider store={store}>
