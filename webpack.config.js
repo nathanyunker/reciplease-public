60322ab98b06a471c420ce2f2eb45bb7b1fd9cbc
Nathan Yunker <nathanyunker@rocketmail.com>
2017-09-26 00:11:20 -0700

    troubleshooting environment

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
 
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify ('development'),
+      'process.env.NODE_ENV': JSON.stringify ('production'),
     }),
   ],
 
