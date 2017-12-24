9266b9328ee022e77db720e90ac82f109b0e11b1
Nathan Yunker <nathanyunker@rocketmail.com>
2017-12-24 01:47:19 -0800

    trouble shooting response message

--- src/scenes/SignIn/SignIn.js
+++ src/scenes/SignIn/SignIn.js
@@ -1,33 +1,139 @@
 import React from 'react';
+import styles from './signIn.less';
 import remove from 'lodash/remove'
 import { connect } from 'react-redux'
-import { Button, Col, ControlLabel, FormControl, FormGroup, ListGroup, ListGroupItem, Row } from 'react-bootstrap';
+import { Button, Checkbox, Col, ControlLabel, FormControl, FormGroup, Form } from 'react-bootstrap';
  
 @connect((store) => {
   return {
   }
 }) 
 class SignIn extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+
     this.state = {
-      
+      password: '',
+      username: ''
     };
+
+    this.authenticate = this.authenticate.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
   }
 
   componentWillMount() {
-
+    // this.setState({
+    //   password: '',
+    //   username: ''
+    // });
   }
 
-  authenticate() {
-    console.log('attempt login');
+  signUp() {
+    console.log('send in the user name and password');
+  }
+
+  authenticate(e) {
+    e.preventDefault();
+    const endpoint = 'http://localhost:3000/login'
+    console.log('THE STATE', this);
+
+    const formPayload = {
+      username: this.state.username,
+      password: this.state.password
+    };
+
+    console.log('THE USER NAME', formPayload.username);
+    console.log('THE PASSWORD', formPayload.password);
+
+    let myHeaders = new Headers({
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    });
+    
+    let request = {
+      method: 'POST',
+      headers: myHeaders,
+      mode: 'cors',
+      body: JSON.stringify(formPayload)
+    };
+
+    fetch(endpoint, request).then(function(response) {
+      console.log('THE RESPONSE', response);
+      return response.json();
+    })
+    .then(function(data) {
+      console.log('HOOORAY WE SHOULD GET A TOKEN BACK IN DATA', data);
+    });
+
+    this.handleClearForm(e);
+  }
+
+  handleClearForm(e) {
+    e.preventDefault();
+    this.setState({
+      username: '',
+      password: ''
+    });
+  }
+
+  handlePasswordChange(e) {
+    this.setState({ password: e.target.value });
+  }
+
+  handleUsernameChange(e) {
+    this.setState({ username: e.target.value });
   }
 
   render() {
     return (
       <div className="container">
-        <span>convert following form into reusable login form</span>
-        <Button className="btn pull-right" onClick={(e) => this.authenticate()}>Login</Button>
+        <div className="half-width"> 
+          <Form horizontal onSubmit={this.authenticate}>
+            <FormGroup controlId="formHorizontalUsername">
+              <Col componentClass={ControlLabel} sm={2} >
+                Username
+              </Col>
+              <Col sm={10}>
+                <FormControl autoComplete="off"
+                            onChange={this.handleUsernameChange}
+                            type="text"
+                            placeholder="Username"
+                            value={this.state.username}/>
+              </Col>
+            </FormGroup>
+
+            <FormGroup controlId="formHorizontalPassword">
+              <Col componentClass={ControlLabel} sm={2}>
+                Password
+              </Col>
+              <Col sm={10}>
+                <FormControl type="password" 
+                              placeholder="Password" 
+                              autoComplete="off"
+                              onChange={this.handlePasswordChange}
+                              value={this.state.password}/>
+              </Col>
+            </FormGroup>
+
+            <FormGroup>
+              <Col smOffset={2} sm={10}>
+                <Checkbox>Remember me</Checkbox>
+              </Col>
+            </FormGroup>
+
+            <FormGroup>
+              <Col smOffset={2} sm={10}>
+                <Button type="submit" >
+                  Sign in
+                </Button>
+              </Col>
+            </FormGroup>
+          </Form>
+          <div>
+            <span onClick={() => SignIn.signUp()}>Not a member? Click Here to become one!</span>
+          </div>
+        </div>
       </div>
     );
   }
