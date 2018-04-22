37730a921e9a80097fa20044b41ee2c93e184ae8
Nathan Yunker <nathanyunker@rocketmail.com>
2018-04-22 01:37:31 -0700

    basic webpack, babel, react setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,32 @@
-const path = require('path');
- 
+const webpack = require('webpack');
+
 module.exports = {
-  context: path.join(__dirname, 'src'),
-  devtool: 'source-map',
   entry: [
-    'whatwg-fetch',
-    './main.js',
+    'react-hot-loader/patch',
+    './src/main.js'
   ],
-  output: {
-    path: path.join(__dirname, 'www'),
-    filename: 'bundle.js',
-  },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            plugins: ['transform-decorators-legacy']
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          { loader: "style-loader" },
-          { loader: "css-loader" }
-        ]
-      },
-      {
-        test: /\.less$/,
-        use: [
-          {loader: "style-loader"}, 
-          {loader: "css-loader"}, 
-          {loader: "less-loader"}
-        ]
-      },
-      { 
-        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader: 'file-loader',
+        use: ['babel-loader']
       }
-    ],
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
   }
 };
