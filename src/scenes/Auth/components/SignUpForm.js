1f6ac8d1e4fa9f3068fdae9c07890d2af179df9f
Nathan Yunker <nathanyunker@rocketmail.com>
2018-09-22 22:15:14 -0700

    removed unused imports, updated reactstrap classes, updated less for auth

--- src/scenes/Auth/components/SignUpForm.js
+++ src/scenes/Auth/components/SignUpForm.js
@@ -66,7 +66,7 @@ class SignUpForm extends Component {
     render() {
         return (
           <Form horizontal onSubmit={this.signUp}>
-              <FormGroup controlid="formHorizontalEmail">
+              <FormGroup controlid="formHorizontalEmail" className="row">
                 <Col componentClass={ControlLabel} sm={2} >
                   Email
                 </Col>
@@ -78,7 +78,7 @@ class SignUpForm extends Component {
                               value={this.state.email}/>
                 </Col>
               </FormGroup>
-              <FormGroup controlid="formHorizontalPassword">
+              <FormGroup controlid="formHorizontalPassword" className="row">
                 <Col componentClass={ControlLabel} sm={2}>
                   Password
                 </Col>
@@ -90,7 +90,7 @@ class SignUpForm extends Component {
                                 value={this.state.password}/>
                 </Col>
               </FormGroup>
-              <FormGroup controlid="formHorizontalPasswordConfirm">
+              <FormGroup controlid="formHorizontalPasswordConfirm" className="row">
                 <Col componentClass={ControlLabel} sm={2}>
                   Confirm Password
                 </Col>
