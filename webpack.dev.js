99a390bfcf90cac911c0b16d0e16125076498431
Nathan Yunker <nathanyunker@rocketmail.com>
2018-11-03 18:51:22 -0700

    experiementing with port and wepack configs

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-console.log('wepack.dev.js---------------------');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,7 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const port = (process.env.PORT || 8080);
-
-console.log(`HERE IS OUR PORT: ${port}`);
-console.log('wepack.prod.js---------------------');
+const port = (process.env.PORT || 8080)
 
 module.exports = merge(common, {
   mode: 'production',
